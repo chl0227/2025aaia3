d79aee66bc72ee4afa98aec4fc373f2eaffd4d98
龍一 <chunlong0227@gmail.com>
2025-10-02 17:21:45 +0800

    week04

---
+++ week04/week04-4a.cpp
@@ -0,0 +1,14 @@
+/// week04-4a.cpp Par1:Input Part2:Output
+/// UVA 100 The 3n + 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b; /// Part1: Input
+	while( cin >> a >> b){
+		int ans = 0;
+		/// ans will be here!
+
+		printf("%d %d %d\n", a, b, ans);
+	} /// Part2: Output
+}
